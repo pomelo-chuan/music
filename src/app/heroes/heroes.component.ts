3188d272ad0b9beb7f99a55f9fa4cd174cd2766c
chenchuan <chuan1992@hotmail.com>
2018-10-09 22:44:09 +0800

    :art:

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mocks-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
